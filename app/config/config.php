93a02c82d41fbe5a9e21fc93cb7ed919172d3db3
perez-mj <perezmj012@gmail.com>
2025-09-14 21:19:58 +0800

    base_url & env changed

--- app/config/config.php
+++ app/config/config.php
@@ -64,7 +64,7 @@ $config['VERSION']                 = '4.2.2';
 | -------------------------------------------------------------------
 | Values: development and production
 */
-$config['ENVIRONMENT']             = 'development';
+$config['ENVIRONMENT']             = 'production';
 
 /*
 |--------------------------------------------------------------------------
@@ -79,7 +79,7 @@ $config['ENVIRONMENT']             = 'development';
 | WARNING: You MUST set this value!
 |
 */
-$config['base_url'] 				= 'http://localhost:3000/';
+$config['base_url'] 				= 'https://perez-markjoseph.onrender.com/';
 
 /*
 |--------------------------------------------------------------------------
--- app/config/database.php
+++ app/config/database.php
@@ -57,13 +57,26 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 |   Example: $database['another_example'] = array('key' => 'value')
 */
 
-$database['main'] = array(
+/*$database['main'] = array(
     'driver'	=> 'mysql',
     'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
     'username'	=> 'sql12798618',
     'password'	=> '2LUy4NggRK',
     'database'	=> 'sql12798618',
+    'charset'	=> 'utf8',
+    'dbprefix'	=> '',
+    // Optional for SQLite
+    'path'      => ''
+);*/
+
+$database['main'] = array(
+    'driver'	=> 'mysql',
+    'hostname'	=> 'localhost',
+    'port'		=> '3306',
+    'username'	=> 'root',
+    'password'	=> '',
+    'database'	=> 'mockdata',
     'charset'	=> 'utf8',
     'dbprefix'	=> '',
     // Optional for SQLite
