627d960ef1c2513d3b9d8710a0a425c3bccb9810
perez-mj <perezmj012@gmail.com>
2025-09-14 20:35:11 +0800

    Dockerfile added

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'bc5dc7b7d9bf1b674b3fee0e2eb5b32c2c988852',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'bc5dc7b7d9bf1b674b3fee0e2eb5b32c2c988852',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
